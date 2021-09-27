cd0009bc2a65d4252e912c9a9d75ed1487701d46
unknown <onuohauchenna15@gmail.com>
2021-09-26 19:07:04 -0700

    updating code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './Layout';
 import Signin from './Account/Signin';
 import SignUp from './Account/SignUp';
-import OtpPage from './Account/OtpPage';
 import CreatePassword from './Account/CreatePassword';
-import SetupPage from './Account/SetupPage';
-import ImagePage from './Account/ImagePage';
-import StoreType from './Account/StoreType';
-import StoreLocation from './Account/StoreLocation';
-import StoreName from './Account/StoreName';
 import {
   Dashboard,
   Orders,
@@ -20,21 +14,14 @@ import {
   Vendors,
 } from '../pages';
 // import ProtectedRoute from '../helpers/protectedRoute';
-import Notifications from '../pages/notifications';
-import NoteDetails from '../pages/noteDetail';
 
 const App = () => (
   <Router>
     <Switch>
       <Route path="/" component={SignUp} exact  />
       <Route path="/login" component={Signin} exact />
-      <Route path="/otppage" component={OtpPage} exact/>
       <Route path="/createpassword" component={CreatePassword} exact />
-      <Route path="/setuppage" component={SetupPage} exact/>
-      <Route path="/imagepage" component={ImagePage} exact/>
-      <Route path="/storetype" component={StoreType} exact/>
-      <Route path="/storename" component={StoreName} exact/>
-      <Route path="/storelocation" component={StoreLocation} exact />
+      
       <Layout>
         <Route path="/dashboard" component={Dashboard} exact />
         <Route path="/orders" component={Orders} exact />
@@ -43,8 +30,6 @@ const App = () => (
         <Route path="/customers" component={Customers} exact />
         <Route path="/vendors" component={Vendors} exact />
         <Route path="/settings" component={Settings} exact />
-        <Route path="/note-details/:id" component={NoteDetails} exact />
-        <Route path="/notifications" component={Notifications} exact />
       </Layout>
     </Switch>
   </Router>
